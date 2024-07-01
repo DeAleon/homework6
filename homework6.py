d6940aa0bec40c38a59bdeeaea84af0434b5c40c
DeAleon <donnotel@mail.ru>
2024-07-01 14:04:58 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,15 @@
+my_dict = {'Anton': 1998, 'Pascha': 1996, 'Oleg': 1997}
+print(my_dict)
+print(my_dict.get('Anton'))
+print(my_dict.get('Kolya'))
+my_dict.update({'Misha': 2000,
+                'Kostya': 1987})
+print(my_dict.get('Pascha'))
+del my_dict['Pascha']
+print(my_dict)
+
+my_set = {1, 2, "Hello", 3.14, 1, 2}
+print(my_set)
+my_set.add((5, 4))
+my_set.discard('Hello')
+print(my_set)
